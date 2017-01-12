ab5d34c6b1639dab0d991b9104a95573a8a1e1ec
Shinn Kondo <shinn.kondo@gmail.com>
2017-01-11 22:03:40 -0600

    Add some css styles.

--- webApp/src/app/app.component.ts
+++ webApp/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ArchiveService} from './archive.service'
+import {Controls} from '@angular/forms';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
     name = 'Angular'
     statusList: ArchiveJobStatus[]
     errorMessage: string
+    data = {}
     
     constructor(private archiveService: ArchiveService) {}
 
--- webApp/webpack.config.js
+++ webApp/webpack.config.js
@@ -21,10 +21,10 @@ module.exports = [{
     module: {
         rules: [
             { test: /\.ts$/, loaders: ["awesome-typescript-loader", 'angular2-template-loader'] },
-            {
-                test: /\.css$/,
-                loaders: ["css-loader"]
-            },
+            // {
+            //     test: /\.css$/,
+            //     loaders: ["css-loader"]
+            // },
                 /* Embed files. */
             { 
                 test: /\.(html|css)$/, 
