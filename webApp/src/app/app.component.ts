00c9d51b2eef8435d1c36c3a2dbc17a7d9fb29ed
Shinn Kondo <shinn.kondo@gmail.com>
2017-01-11 22:12:30 -0600

    Fix to remove wrong import statement.

--- webApp/src/app/app.component.ts
+++ webApp/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ArchiveService} from './archive.service'
-import {Controls} from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
     name = 'Angular'
