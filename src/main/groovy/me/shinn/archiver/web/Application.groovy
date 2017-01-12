48b6d7ba910cf1e5e66c2e5caf30d3fe62962a4b
Shinn Kondo <shinn.kondo@gmail.com>
2017-01-11 23:26:35 -0600

    Introduce sprint devtool for fast dev iteration.

--- src/main/groovy/me/shinn/archiver/web/Application.groovy
+++ src/main/groovy/me/shinn/archiver/web/Application.groovy
@@ -3,6 +3,7 @@ package me.shinn.archiver.web
 import me.shinn.archiver.core.CoreManager
 import me.shinn.archiver.core.config.ModuleBundle
 import me.shinn.archiver.web.archivable.ArchivableManager
+import me.shinn.archiver.web.socket.ArchivableUpdateHandler
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
@@ -21,5 +22,10 @@ class Application {
     @Bean
     CoreManager coreManager() {
         return ModuleBundle.injector.getInstance(CoreManager.class)
+    }
+
+    @Bean
+    ArchivableUpdateHandler archivableUpdateHandler() {
+        return new ArchivableUpdateHandler()
     }
 }
